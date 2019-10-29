273fe02b293a0003287182969b43a7d2e3db230c
Lucyna Borkowska <lucyna.borkowska@mail.com>
2019-10-29 15:02:22 +0100

    Form Array sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { SummaryPipe } from './summary.pipe';
 import { InputFormatDirective } from './input-format.directive';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NewCourseFormComponent } from './signup-form/new-course-form.component.ts/new-course-form.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CoursesComponent,
     SummaryPipe,
     InputFormatDirective,
+    NewCourseFormComponent
     
   ],
   imports: [
