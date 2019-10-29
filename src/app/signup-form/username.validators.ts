d41cc5b17d9aef3f6dd3481306922787dc66c099
Lucyna Borkowska <lucyna.borkowska@mail.com>
2019-10-29 13:44:23 +0100

    Checking for unique name

--- src/app/signup-form/username.validators.ts
+++ src/app/signup-form/username.validators.ts
@@ -1,10 +1,20 @@
-import {AbstractControl, ValidationErrors} from "@angular/forms";
+import {AbstractControl, ValidationErrors, ControlContainer} from "@angular/forms";
 
 export class UsernameValidators {
-  static cannotContainSpace(control: AbstractControl) : ValidationErrors | null {
-    if ((control.value as string).indexOf('') >= 0)
-    return { cannotContainSpace: true };
+  static cannotContainSpace(control: AbstractControl) : Promise < ValidationErrors | null> {
+    if ((control.value as string).indexOf('') >= 0);
+    return {cannotContainSpace: true };
 
     return null;
+  }
+  static shouldBeUnique(control: AbstractControl) :  Promise < ValidationErrors | null >{
+    return new Promise((resolve, reject) => {
+      setTimeout( () => {
+        if (control.value === "lucy")
+        resolve ({ shouldBeUnique: true});
+        else 
+        resolve(null);
+      }, 2000 );
+    });
   }
 }
