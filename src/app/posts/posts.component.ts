37c8c794ae9cde0c6efba03a2f8e5548ccb9fd51
Lucyna Borkowska <lucyna.borkowska@mail.com>
2019-10-30 11:09:15 +0100

    Creating Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -10,12 +10,22 @@ import { Injectable } from '@angular/core';
 })
 export class PostsComponent {
   posts: any;
+  private url = "https://jsonplaceholder.typicode.com/posts";
   constructor(private http: HttpClient) {
-    http.get('https://jsonplaceholder.typicode.com/posts')
+    http.get(this.url)
     .subscribe(response => {
       this.posts = response;
     });
   }
-
+  createPost(input: HTMLInputElement) {
+    let post: any = {title: input.value};
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe( response => {
+        //post['id'] = response.id;
+        this.posts.splice(0,0, post);
+        console.log(response);
+      });
+  }
   }
 
